7b3b4659d41104a1452386b0ba644ca9929c56d4
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2025-12-14 21:56:51 -0300

    Feat: criar página pedidos.html e atualizar vite.config

--- magazine/vite.config.js
+++ magazine/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'index.html'),
         checkout: path.resolve(__dirname, 'checkout.html'),
+        pedidos: path.resolve(__dirname, 'pedidos.html'),
       },
     },
   },
