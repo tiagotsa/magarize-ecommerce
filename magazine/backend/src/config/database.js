13919ff255d162473c0277d358e19ed42d752fc7
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2026-05-23 19:06:12 -0300

    DEPLOY RENDER

--- magazine/backend/src/config/database.js
+++ magazine/backend/src/config/database.js
@@ -8,9 +8,12 @@ dotenv.config();
 const connectionString = process.env.DATABASE_URL || 
   `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
+// Detecta se a aplicação está rodando no ambiente do Render ou em produção
+const isProduction = process.env.RENDER === 'true' || process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   connectionString,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 export { pool };
