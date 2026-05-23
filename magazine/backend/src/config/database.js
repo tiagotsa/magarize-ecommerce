d5b4e79fcc25aea3016d90632353a0bf1fe60c71
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2026-05-23 18:13:01 -0300

    Deploy Render

--- magazine/backend/src/config/database.js
+++ magazine/backend/src/config/database.js
@@ -1,14 +1,30 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const { Pool } = pg
+let dbConfig;
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
+// Se existir a variável DATABASE_URL (Ou seja, estamos rodando no Render)
+if (process.env.DATABASE_URL) {
+  dbConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false // ISSO AQUI RESOLVE O ERRO SSL/TLS!
+    }
+  };
+} else {
+  // Se não existir, estamos rodando localmente no seu computador
+  dbConfig = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  };
+}
+
+const pool = new Pool(dbConfig);
+
+export { pool };
